cfc00cb4df3f939ef0157bf2c7f4c988d7d7ec66
CountCook <kirillfield94@gmail.com>
2025-01-07 01:39:07 +0200

    version2

--- Module-2/05012025-calculator/app.ts
+++ Module-2/05012025-calculator/app.ts
@@ -2,3 +2,5 @@ const number1 = Number(prompt("what is your number?"));
 alert ("pick a number");
 const number2 = Number(prompt("what is your number?"));
 alert ("pick a number");
+const number3 = Number(prompt("what is your number?"));
+alert ("pick a number");
--- Module-2/05012025-calculator/dist/app.js
+++ Module-2/05012025-calculator/dist/app.js
@@ -2,3 +2,5 @@ var number1 = Number(prompt("what is your number?"));
 alert("pick a number");
 var number2 = Number(prompt("what is your number?"));
 alert("pick a number");
+var number3 = Number(prompt("what is your number?"));
+alert("pick a number");
