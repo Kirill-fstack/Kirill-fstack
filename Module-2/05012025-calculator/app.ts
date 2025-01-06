5b940d0b8f837299591265bfa21e2c9b705d854b
Kirill station <kirillfield94@gmail.com>
2025-01-07 01:55:19 +0200

    just hcecking

--- Module-2/05012025-calculator/app.ts
+++ Module-2/05012025-calculator/app.ts
@@ -2,5 +2,3 @@ const number1 = Number(prompt("what is your number?"));
 alert ("pick a number");
 const number2 = Number(prompt("what is your number?"));
 alert ("pick a number");
-const number3 = Number(prompt("what is your number?"));
-alert ("pick a number");
--- Module-2/05012025-calculator/dist/app.js
+++ Module-2/05012025-calculator/dist/app.js
@@ -2,5 +2,3 @@ var number1 = Number(prompt("what is your number?"));
 alert("pick a number");
 var number2 = Number(prompt("what is your number?"));
 alert("pick a number");
-var number3 = Number(prompt("what is your number?"));
-alert("pick a number");
